e780418b1359b786dd4fbc3c097a7d7cf28ca04b
Kailey <kailey.izard@gmail.com>
2016-09-10 07:35:50 -0400

    precompile assets

---
+++ public/assets/scripts_EB-ae9bac851d9dbf8df300e71d5c9277139b5d7bb4b49348df688125c7ab649003.js
@@ -0,0 +1,27 @@
+$(function(){
+
+// Owl Carousel Moore-Place 
+
+	// if($(window).width() < 768){
+	// 	$(".moore-carousel").owlCarousel({
+ 
+	//       autoPlay: 3000, //Set AutoPlay to 3 seconds
+	 
+	//       items : 1,
+	//       dots: true,
+	//       autoHeight : true,
+	//       loop: true,
+	//       itemsDesktop : [1199,3],
+	//       itemsDesktopSmall : [979,3]
+		 
+	// 	});
+	// } else {
+	// 	$(".moore-carousel").removeClass('owl-carousel')
+	// }
+
+// FancyBox iFrame
+
+	$(".fancybox").fancybox();
+
+	
+});
