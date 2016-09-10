5facc304526e9d13cf1e060c8b175f062e221ec4
Kailey <kailey.izard@gmail.com>
2016-09-10 11:36:22 -0400

    Book header and wuotes

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -2,4 +2,7 @@ module BlogPostsHelper
 	def tag_links(tags)
 	  tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(", ") 
 	end
+	def nl2br(s)
+	  s.gsub(/\n/, '<br>')
+	end
 end
