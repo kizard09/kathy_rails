75fcacd38865068dae327439844ea71d92b593a1
cacure <clinton.a.cure@gmail.com>
2018-07-08 13:16:48 -0400

    clean up

--- test/models/contact_user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ContactUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/speaking_user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SpeakingUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
